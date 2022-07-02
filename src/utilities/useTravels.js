70701f5a8d4034c4eb105b8106a509df90431143
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-02 15:01:30 +0600

    some utilities added

---
+++ src/utilities/useTravels.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const useTravels = () => {
+    const [travels, setTravels] = useState([]);
+    useEffect(() => {
+        const url = `travel.json`;
+        const getTravels = async () => {
+            const request = await fetch(url);
+            const response = await request.json();
+            setTravels(response);
+        };
+        getTravels();
+    }, []);
+
+    return [travels];
+};
+
+export default useTravels;
